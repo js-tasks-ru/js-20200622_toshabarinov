018b1760ccd277ab99ea2782467330cd6476a3c4
Anton Barinov <toshabarinov@gmail.com>
2020-07-01 18:16:06 +0200

    [03-objects-arrays-intro-to-testing] 3-trim-symbols done

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,6 +4,12 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(string, size) {
-
+export function trimSymbols(string, size = string.length) {
+  let charCounter = 1;
+  let prevChar = string.slice(0, 1);
+  return [...string].filter(char => {
+    charCounter = (prevChar === char ? charCounter : 1);
+    prevChar = char;
+    return charCounter++ <= size;
+  }).join('');
 }
